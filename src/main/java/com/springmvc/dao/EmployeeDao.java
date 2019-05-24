41216af5eb165dbda7c38f4cb312a5cc2cd9cb68
John <johnathonmonson@gmail.com>
2019-05-24 12:59:18 -0700

    Implemented prepared statements

--- src/main/java/com/springmvc/dao/EmployeeDao.java
+++ src/main/java/com/springmvc/dao/EmployeeDao.java
@@ -1,9 +1,13 @@
 package com.springmvc.dao;
 
 import com.springmvc.dto.Employee;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -43,15 +47,70 @@ public class EmployeeDao  {
     }
 	
 	public Employee getEmployee(String id){
-                System.out.println("*************************ID: " + id);
-		String sql = "Select EMPID, NAME,STATUS,TENURE,PHONE,EMAIL,JOINING_DATE,WORKLOC,CURRENTLOC,HOMELOC,ISRELOCATE,ROLEID,VERTICALID,ACCOUNTID from information_schema.employee where EMPID = ?::integer";
-		Employee employee = (Employee) jdbcTemplate.queryForObject(sql, new Object[] { id }, employeeMapper);
-                return employee;
+       
+            try {
+                PreparedStatement ps = jdbcTemplate.getDataSource().getConnection().prepareStatement("select * from information_schema.employee where empid = ?");
+                ps.setInt(1, Integer.parseInt(id));
+                
+                Employee e = new Employee();
+                
+                ResultSet rs = ps.executeQuery();
+                while(rs.next()){
+                    e.setEmpId(rs.getString("EMPID"));
+                    e.setName(rs.getString("NAME"));
+                    e.setStatus(rs.getString("STATUS"));
+                    e.setTenure(rs.getString("TENURE"));
+                    e.setPhone(rs.getString("PHONE"));
+                    e.setEmail(rs.getString("EMAIL"));
+                    e.setDoj(rs.getString("JOINING_DATE"));
+                    e.setWl(rs.getString("WORKLOC"));
+                    e.setCl(rs.getString("CURRENTLOC"));
+                    e.setHl(rs.getString("HOMELOC"));
+                    e.setRmid(rs.getString("ISRELOCATE"));
+                    e.setRoleid(rs.getString("ROLEID"));
+                    e.setVertid(rs.getString("VERTICALID"));
+                    e.setAcctid(rs.getString("ACCOUNTID"));
+                }
+                return e;
+            } catch (SQLException ex) {
+                Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Employee e2 = new Employee();
+            return e2;
 	}
 	
 	public List<Employee> getEmployees(){
-		String sql = "Select EMPID, NAME,STATUS,TENURE,PHONE,EMAIL,JOINING_DATE,WORKLOC,CURRENTLOC,HOMELOC,ISRELOCATE,ROLEID,VERTICALID,ACCOUNTID from information_schema.employee";
-		return jdbcTemplate.query(sql, employeeMapper);
+            try {
+                PreparedStatement ps = jdbcTemplate.getDataSource().getConnection().prepareStatement("select * from information_schema.employee");
+                List<Employee> employees = new ArrayList();
+                
+                ResultSet rs = ps.executeQuery();
+                while(rs.next()){
+                    Employee e = new Employee();
+                    e.setEmpId(rs.getString("EMPID"));
+                    e.setName(rs.getString("NAME"));
+                    e.setStatus(rs.getString("STATUS"));
+                    e.setTenure(rs.getString("TENURE"));
+                    e.setPhone(rs.getString("PHONE"));
+                    e.setEmail(rs.getString("EMAIL"));
+                    e.setDoj(rs.getString("JOINING_DATE"));
+                    e.setWl(rs.getString("WORKLOC"));
+                    e.setCl(rs.getString("CURRENTLOC"));
+                    e.setHl(rs.getString("HOMELOC"));
+                    e.setRmid(rs.getString("ISRELOCATE"));
+                    e.setRoleid(rs.getString("ROLEID"));
+                    e.setVertid(rs.getString("VERTICALID"));
+                    e.setAcctid(rs.getString("ACCOUNTID"));
+                    employees.add(e);
+                }
+                
+                System.out.println(rs.toString());
+                return employees;
+            } catch (SQLException ex) {
+                Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            List<Employee> employees2 = new ArrayList();
+            return employees2;
 	}
 
     private static final RowMapper<Employee> employeeMapper = new RowMapper<Employee>() {
