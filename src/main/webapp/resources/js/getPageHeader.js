38b783f255b8ccfd8c562cdec38a3eeb12329c90
Abdelhalim <golden-boy1994@hotmail.fr>
2019-07-20 14:39:54 -0700

    fixed context issue with links

--- src/main/webapp/resources/js/getPageHeader.js
+++ src/main/webapp/resources/js/getPageHeader.js
@@ -5,10 +5,10 @@ class PageHeader extends React.Component {
             <div className="nav-container">
             <img src={resourcePath.href+"/resources/img/logo.png"} alt="logo" className="logoImg"></img>
                 <ul>
-                    <li><a href="/springMVC/">Home</a></li>
-                    <li><a href="/springMVC/employee/table">1) Employee Table</a></li>
-                    <li><a href="/springMVC/employee/map">2) Employee Map</a></li>
-                    <li><a href="/springMVC/job/map">3) Job Map</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/employee/table">1) Employee Table</a></li>
+                    <li><a href="/employee/map">2) Employee Map</a></li>
+                    <li><a href="/job/map">3) Job Map</a></li>
                 </ul>
             </div>
         );
